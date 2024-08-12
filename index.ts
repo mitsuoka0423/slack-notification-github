4ab11d6d245955ba3d46a445c11c3ba4017ba941
光岡 高宏/MITSUOKA Takahiro <61106530+mitsuoka0423@users.noreply.github.com>
2024-08-12 02:58:22 +0000

    feat: bun

--- index.ts
+++ index.ts
@@ -1,43 +1,71 @@
+import fs from 'node:fs';
+import http from 'node:http';
 import { App } from '@octokit/app';
-import { serve } from 'bun';
+import { createNodeMiddleware } from '@octokit/webhooks';
 
-if (!process.env.GITHUB_APP_ID) {
-	throw new Error('環境変数にGITHUB_APP_IDを設定してください');
-}
-
-if (!process.env.GITHUB_PRIVATE_KEY) {
-	throw new Error('環境変数にGITHUB_PRIVATE_KEYを設定してください');
-}
-
-if (!process.env.GITHUB_WEBHOOK_SECRET) {
-	throw new Error('環境変数にGITHUB_WEBHOOK_SECRETを設定してください');
-}
+const appId = process.env.APP_ID || '';
+const privateKeyPath = process.env.PRIVATE_KEY_PATH || '';
+const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
+const secret = process.env.WEBHOOK_SECRET || '';
+const enterpriseHostname = process.env.ENTERPRISE_HOSTNAME;
+// const messageForNewPRs = fs.readFileSync('./message.md', 'utf8');
 
 const app = new App({
-	appId: process.env.GITHUB_APP_ID,
-	privateKey: process.env.GITHUB_PRIVATE_KEY,
+	appId,
+	privateKey,
 	webhooks: {
-		secret: process.env.GITHUB_WEBHOOK_SECRET,
+		secret,
 	},
+	// NOTE: GHESを利用する場合は設定する
+	// ...(enterpriseHostname && {
+	// 	Octokit: Octokit.defaults({
+	// 		baseUrl: `https://${enterpriseHostname}/api/v3`,
+	// 	}),
+	// }),
 });
+
+const { data } = await app.octokit.request('/app');
+
+app.octokit.log.debug(`Authenticated as '${data.name}'`);
 
 app.webhooks.on('pull_request.opened', async ({ octokit, payload }) => {
-	const { repository, pull_request } = payload;
-
-	console.log('新しいPRが作成されました！');
-	console.log('----------------------------');
-	console.log(`リポジトリ: ${repository.full_name}`);
-	console.log(`タイトル: ${pull_request.title}`);
-	console.log(`作成者: ${pull_request.user.login}`);
-	console.log(`URL: ${pull_request.html_url}`);
-	console.log('----------------------------');
+	console.log(
+		`Received a pull request event for #${payload.pull_request.number}`,
+	);
+	console.log(JSON.stringify(payload, null, 2));
+	// try {
+	// 	await octokit.rest.issues.createComment({
+	// 		owner: payload.repository.owner.login,
+	// 		repo: payload.repository.name,
+	// 		issue_number: payload.pull_request.number,
+	// 		body: messageForNewPRs,
+	// 	});
+	// } catch (error) {
+	// 	if (error.response) {
+	// 		console.error(
+	// 			`Error! Status: ${error.response.status}. Message: ${error.response.data.message}`,
+	// 		);
+	// 	} else {
+	// 		console.error(error);
+	// 	}
+	// }
 });
 
-const server = serve({
-	fetch: async (request) => {
-		return new Response('OK');
-	},
-	port: process.env.PORT || 3000,
+app.webhooks.onError((error) => {
+	if (error.name === 'AggregateError') {
+		console.log(`Error processing request: ${error.event}`);
+	} else {
+		console.log(error);
+	}
 });
 
-console.log(`サーバーが起動しました: ${server.url}`);
+const port = process.env.PORT || 3000;
+const path = '/api/webhook';
+const localWebhookUrl = `http://localhost:${port}${path}`;
+
+const middleware = createNodeMiddleware(app.webhooks, { path });
+
+http.createServer(middleware).listen(port, () => {
+	console.log(`Server is listening for events at: ${localWebhookUrl}`);
+	console.log('Press Ctrl + C to quit.');
+});
